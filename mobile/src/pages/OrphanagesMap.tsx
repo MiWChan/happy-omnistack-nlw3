8c6c86dea414380a4a6b3e24ab40a593341279e1
MiWChan <michelechan1996@gmail.com>
2020-10-21 19:42:55 -0300

    App finished

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,20 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
-import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
-import { Feather } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from "react-native-maps";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
+import { RectButton } from "react-native-gesture-handler";
 
-import mapMarker from '../images/map-marker.png';
+import mapMarker from "../images/map-marker.png";
+import api from "../services/api";
 
-export default function OrphanagesMa() {
-    const navigation = useNavigation();
+interface Orphanage {
+  id: number;
+  name: string;
+  latitude: number,
+  longitude: number, 
+}
 
-    function handleNavigateToOrphanageDetails() {
-        navigation.navigate('OrphanageDetails');
-    }
+export default function OrphanagesMap() {
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+  const navigation = useNavigation();
 
-    return (
-        <View style={styles.container}>
+  //console.log(orphanages)
+
+  useFocusEffect(() => {
+    api.get("orphanages").then((response) => {
+      setOrphanages(response.data);
+    });
+  });
+
+  function handleNavigateToOrphanageDetails(id: number) {
+    navigation.navigate("OrphanageDetails", { id });
+  }
+
+  function handleNavigateToCreateOrphanage() {
+    navigation.navigate("SelectMapPosition");
+  }
+
+  return (
+    <View style={styles.container}>
       <MapView
         provider={PROVIDER_GOOGLE}
         style={styles.map}
@@ -25,91 +47,99 @@ export default function OrphanagesMa() {
           longitudeDelta: 0.008,
         }}
       >
-        <Marker
-          icon={mapMarker}
-          calloutAnchor={{
-            x: 2.7,
-            y: 0.8,
-          }}
-          coordinate={{
-            latitude: -27.2092052,
-            longitude: -49.6401092,
-          }}
-        >
-          <Callout tooltip onPress={handleNavigateToOrphanageDetails}>
-            <View style={styles.calloutContainer}>
-              <Text style={styles.calloutText}>Lar das meninas</Text>
-            </View>
-          </Callout>
-        </Marker>
+        {orphanages.map( orphanage => {
+          return (
+            <Marker
+              key={orphanage.id}
+              icon={mapMarker}
+              calloutAnchor={{
+                x: 2.7,
+                y: 0.8,
+              }}
+              coordinate={{
+                latitude: orphanage.latitude,
+                longitude: orphanage.longitude,
+              }}
+            >
+              <Callout tooltip onPress={() => handleNavigateToOrphanageDetails(orphanage.id)}>
+                <View style={styles.calloutContainer}>
+            <Text style={styles.calloutText}>{orphanage.name}</Text>
+                </View>
+              </Callout>
+            </Marker>
+          );
+        })}
       </MapView>
 
       <View style={styles.footer}>
-        <Text style={styles.footerText}>2 orfanatos encontrados</Text>
-        
-        <TouchableOpacity style={styles.createOrphanageButton} onPress={() => {}}>
+        <Text style={styles.footerText}>{orphanages.length} orfanatos encontrados</Text>
+
+        <RectButton
+          style={styles.createOrphanageButton}
+          onPress={handleNavigateToCreateOrphanage}
+        >
           <Feather name="plus" size={20} color="#FFF" />
-        </TouchableOpacity> 
-      </View>      
+        </RectButton>
+      </View>
     </View>
-    );
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-  
-    map: {
-      width: Dimensions.get('window').width,
-      height: Dimensions.get('window').height,
-    },
-  
-    calloutContainer: {
-      width: 160,
-      height: 46,
-      paddingHorizontal: 16,
-      backgroundColor: 'rgba(255, 255, 255, 0.8)',
-      borderRadius: 16,
-      justifyContent: 'center',
-    },
-  
-    calloutText: {
-      color: '#0089a5',
-      fontSize: 14,
-      fontFamily: 'Nunito_700Bold',
-    },
-  
-    footer: {
-      position: 'absolute',
-      left: 24,
-      right: 24,
-      bottom: 32,
-  
-      backgroundColor: '#FFF',
-      borderRadius: 20,
-      height: 56,
-      paddingLeft: 24,
-  
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-  
-      elevation: 3,
-    },
-    
-    footerText: {
-      fontFamily: 'Nunito_700Bold',
-      color: '#8fa7b3',
-    },
-    
-    createOrphanageButton: {
-      width: 56,
-      height: 56,
-      backgroundColor: '#15c3d6',
-      borderRadius: 20,
-  
-      justifyContent: 'center',
-      alignItems: 'center',
-      }
-  });
+  container: {
+    flex: 1,
+  },
+
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    borderRadius: 16,
+    justifyContent: "center",
+  },
+
+  calloutText: {
+    color: "#0089a5",
+    fontSize: 14,
+    fontFamily: "Nunito_700Bold",
+  },
+
+  footer: {
+    position: "absolute",
+    left: 24,
+    right: 24,
+    bottom: 32,
+
+    backgroundColor: "#FFF",
+    borderRadius: 20,
+    height: 56,
+    paddingLeft: 24,
+
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+
+    elevation: 3,
+  },
+
+  footerText: {
+    fontFamily: "Nunito_700Bold",
+    color: "#8fa7b3",
+  },
+
+  createOrphanageButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: "#15c3d6",
+    borderRadius: 20,
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
